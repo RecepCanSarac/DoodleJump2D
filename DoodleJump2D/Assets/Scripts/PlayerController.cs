c7377896200837e3e3c4a3895a0af3c079c0535e
Recep Can Saraç <113214739+RecepCanSarac@users.noreply.github.com>
2023-03-29 23:42:02 +0300

    Player FaceFlip Okay

--- DoodleJump2D/Assets/Scripts/PlayerController.cs
+++ DoodleJump2D/Assets/Scripts/PlayerController.cs
@@ -7,15 +7,27 @@ public class PlayerController : MonoBehaviour
     public float speed;
     public float Force;
     private Rigidbody2D rb;
+    private bool lookRight = true;
+    SpriteRenderer spriteRenderer;
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
   
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.A) && lookRight)
+        {
+            spriteRenderer.flipX = true;
+            lookRight = false;
+        }
+        if (Input.GetKeyDown(KeyCode.D) && !lookRight)
+        {
+            spriteRenderer.flipX = false;
+            lookRight = true;
+        }
     }
     private void FixedUpdate()
     {
