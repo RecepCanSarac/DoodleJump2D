6c3b1ad8eacb8bc8f13dbafb3d423e8b5d3df63e
Recep Can Saraç <113214739+RecepCanSarac@users.noreply.github.com>
2023-03-29 23:36:10 +0300

    Platform Player Force

--- DoodleJump2D/Assets/Scripts/PlayerController.cs
+++ DoodleJump2D/Assets/Scripts/PlayerController.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public float speed;
+    public float Force;
     private Rigidbody2D rb;
     void Start()
     {
@@ -20,5 +21,13 @@ public class PlayerController : MonoBehaviour
     {
         float horizontalMove = Input.GetAxisRaw("Horizontal");
         rb.velocity = new Vector2(horizontalMove * speed * Time.deltaTime,rb.velocity.y);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("platform"))
+        {
+            rb.AddForce(transform.up * Force);
+        }
     }
 }
